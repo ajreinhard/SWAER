57efe31d294afc08abb958a3f624e22afac6b275
Anthony <anthony.reinhard@otterbein.edu>
2018-09-06 13:37:58 -0400

    com

---
+++ other/tweeting.R
@@ -0,0 +1,8 @@
+install.packages("twitteR")
+??twitteR
+
+library(twitteR)
+setup_twitter_oauth('3OFet2Lrb7SM3P5xRIS4mYcYT', 'lUY751Alq4eneCod71LwCG3L4PT4L20PMET8sGqKgovrZOfbdg', access_token='981969608277135360-7w95AsxJKN6gbCBogRcLiawZ2znXoIs', access_secret='JEmeS2Gs2TavP5DEwwJgJWGH0ryzjHZBpGqpZG6jk9ZIK')
+
+updateStatus('Hey @reinhurdler, pickup the pace!',mediaPath='C:/Users/Owner/Documents/GitHub/SWAER/other/SavedResults.jpg')
+
